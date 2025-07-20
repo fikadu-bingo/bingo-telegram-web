5823f4675ff63fc75cbe97baae526bb4d621b872
fikadu-bingo <fikadu.worku02@gmail.com>
2025-07-20 12:56:39 +0300

    Add files via upload
    AgentLogin is added because to deploy agent dashboard on vercel

---
+++ src/pages/AgentLogin.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function AgentLogin() {
+  return (
+    <div style={{ padding: "40px", textAlign: "center" }}>
+      <h2>Agent Login Page</h2>
+      <p>This route is working!</p>
+    </div>
+  );
+}
+
+export default AgentLogin;
