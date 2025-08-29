e985d9e3752df2165d993e58a8c6662594bf9eb1
fikadu-bingo <fikadu.worku02@gmail.com>
2025-08-29 11:34:14 +0300

    Add files via upload

--- src/components/CartelaModal.js
+++ src/components/CartelaModal.js
@@ -20,12 +20,9 @@ const CartelaModal = ({ isOpen, onClose, cartelaData, title }) => {
           <div className="cartela-modal-grid">
             {cartelaData.map((row, rowIdx) =>
               row.map((number, colIdx) => (
-               <div
-  key={`${rowIdx}-${colIdx}}
-  className={cartela-modal-cell ${number === "*" ? "star" : ""}`}
->
-  {number}
-</div>
+                <div key={`${rowIdx}-${colIdx}`} className="cartela-modal-cell">
+                  {number}
+                </div>
               ))
             )}
           </div>
